53db5b39fad3ef0a14d8489d56b9e1349ea5af39
Zichao LI <cszichao@gmail.com>
2024-06-03 17:29:17 +0800

    parallel execute

--- parallel.go
+++ parallel.go
@@ -20,8 +20,15 @@ func (p ParallelTaskError[Key]) Error() string {
 // Task 单个任务, 指定任务的 key, 执行失败返回对应的错误
 type Task[Key comparable] func(key Key) error
 
+// TaskFunc 单个任务, 指定任务的 ID 与 key, 执行失败返回对应的错误
+type TaskFunc[Key comparable] func(id int, key Key) error
+
 // Execute 执行一个并发任务，如果有错误则一定返回 ParallelTaskError
 func Execute[Key comparable](keys []Key, thread int, task Task[Key]) error {
+	return ParallelExecute(keys, thread, func(id int, key Key) error { return task(key) })
+}
+
+func ParallelExecute[Key comparable](keys []Key, thread int, task TaskFunc[Key]) error {
 	// 输入检查
 	if len(keys) == 0 || task == nil || thread <= 0 {
 		return nil
@@ -49,7 +56,7 @@ func Execute[Key comparable](keys []Key, thread int, task Task[Key]) error {
 	for i := 0; i < thread; i++ {
 		go func(t int) {
 			for key := range keyChan {
-				if err := task(key); err != nil {
+				if err := task(t, key); err != nil {
 					errMu.Lock()
 					errs[key] = err
 					errMu.Unlock()
@@ -65,13 +72,13 @@ func Execute[Key comparable](keys []Key, thread int, task Task[Key]) error {
 	return errs
 }
 
-func safeTask[Key comparable](t Task[Key]) Task[Key] {
-	return func(key Key) (err error) {
+func safeTask[Key comparable](t TaskFunc[Key]) TaskFunc[Key] {
+	return func(id int, key Key) (err error) {
 		defer func() {
 			if r := recover(); r != nil {
-				err = errors.Errorf("panic: %v\nstack: %s", r, debug.Stack())
+				err = errors.Errorf("task %d panic: %v\nstack: %s", id, r, debug.Stack())
 			}
 		}()
-		return t(key)
+		return t(id, key)
 	}
 }
